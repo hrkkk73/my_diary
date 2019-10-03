25f0ee4a2c3bd7b37e3723ae7ad8c71f78bef6d2
磯谷浩貴 <>
2019-10-03 21:40:31 +0900

    日記作成ページ編集

--- resources/views/diary/create.blade.php
+++ resources/views/diary/create.blade.php
@@ -2,17 +2,20 @@
 @section ('content')
 
 <h2 class="mb-3">日記作成</h2>
+<div class="container">
 {!! Form::open(['route' => 'diary.store']) !!}
-  <div class="form-group">
-    {!! Form::input('date', 'writing_time', null, ['class' => 'form-control']) !!}
-  </div>
-  <div class="form-group">
-    <input type="text" class="form-control" placeholder="日記内容">
-  </div>
-  <div class="">
-        {!! Form::textarea('contents', null, ['class' => 'form-control', 'placeholder' => 'Content']) !!}
-  </div>
-  <button type="submit" class="btn btn-success float-right">追加</button>
-{!! Form::close() !!}
+  {!! Form::input('hidden', 'user_id', Auth::id(), ['class' => 'form-control']) !!}
+    <div class="form-group">
+      {!! Form::input('date', 'writing_time', null, ['class' => 'form-control']) !!}
+    </div>
+    <div class="form-group">
+      {!! Form::input('text', 'title', null, ['class' => 'form-control', 'placeholder' => '日記内容']) !!}
+    </div>
+    <div class="">
+          {!! Form::textarea('contents', null, ['class' => 'form-control', 'placeholder' => 'Content']) !!}
+    </div>
+    <button type="submit" class="btn btn-success float-right">追加</button>
+  {!! Form::close() !!}
+</div>
 
 @endsection
