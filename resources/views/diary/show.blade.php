4c53d166eab816784ff485946d5b15713dedc5ca
磯谷浩貴 <>
2019-10-03 21:41:37 +0900

    日記詳細ページ編集

--- resources/views/diary/show.blade.php
+++ resources/views/diary/show.blade.php
@@ -1,31 +1,31 @@
-@extends ('common.user')
+@extends ('layouts.app')
 @section ('content')
 
-<h1 class="brand-header">日記詳細</h1>
+<h1 class="page-header">日記詳細</h1>
 <div class="main-wrap">
   <div class="panel panel-success">
     <div class="panel-heading">
-      {{ $report->reporting_time->format('Y/m/d (D)') }}&nbsp;&nbsp;の日報
+      {{ $diaries->writing_time }}&nbsp;&nbsp;の日記
     </div>
     <div class="table-responsive">
       <table class="table table-striped table-bordered">
         <tbody>
           <tr>
             <th class="table-column">Title</th>
-            <td class="td-text">{{ $report->title }}</td>
+            <td class="td-text">{{ $diaries->title }}</td>
           </tr>
           <tr>
             <th class="table-column">Content</th>
-            <td class='td-text'>{!! nl2br(e($report->contents)) !!}</td>
+            <td class='td-text'>{!! nl2br(e($diaries->contents)) !!}</td>
           </tr>
         </tbody>
       </table>
     </div>
   </div>
   <div class="btn-bottom-wrapper">
-    <a class="btn btn-edit" href="{{ route('report.edit', $report->id) }}"><i class="fa fa-pencil" aria-hidden="true"></i></a>
+    <a class="btn btn-edit" href=""><i class="fa fa-pencil" aria-hidden="true"></i></a>
     <div class="btn-delete">
-      {!! Form::open(['route' => ['report.destroy', $report->id], 'method' => 'DELETE']) !!}
+      {!! Form::open() !!}
         <button class="btn btn-danger" type="submit"><i class="fa fa-trash-o"></i></button>
       {!! Form::close() !!}
     </div>
