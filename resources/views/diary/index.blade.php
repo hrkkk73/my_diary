8d78a335bd3084d8e66184ceca52b446dec15d54
磯谷浩貴 <>
2019-10-07 10:29:28 +0900

    検索機能を行うためのタグを用意

--- resources/views/diary/index.blade.php
+++ resources/views/diary/index.blade.php
@@ -2,28 +2,36 @@
 @section ('content')
 
 <h1 class="page-header">日記一覧</h1>
-<p class="text-right">
-  <a class="btn btn-success" href="/diary/create">日記を追加</a>
-</p>
-<table class="table">
-  <thead class="thead-light">
-    <tr>
-      <th> Date</th>
-      <th>title</th>
-      <th>content</th>
-      <th></th>
-    </tr>
-  </thead>
-  <tbody>
-  @foreach($diaries as $diary)
-    <tr>
-      <td class="align-middle">{{ $diary->writing_time }}</td>
-      <td class="align-middle">{{ $diary->title }}</td>
-      <td class="align-middle">{{ $diary->contents }}</td>
-      <td class="btn btn-primary"><a class="btn" href="diary/{{ $diary->id }}"><i class="fa fa-book"></i></a></td>
-    </tr>
-  @endforeach
-  </tbody>
-</table>
+<div class="main-wrap diary">
+  <div class="btn-wrapper diary">
+    {!! Form::open(['route' => 'diary.index', 'method' => 'GET']) !!}
+      {!! Form::input('month', 'search-month', empty($inputs['search-month']) ? null : $inputs['search-month'], ['class' => 'form-control']) !!}
+      <button type="submit" class="btn"><i class="fa fa-search"></i></button>
+    {!! Form::close() !!}
+    <p class="text-right">
+      <a class="btn btn-success" href="/diary/create">日記を追加</a>
+    </p>
+  </div>
+  <table class="table">
+    <thead class="thead-light">
+      <tr>
+        <th> Date</th>
+        <th>title</th>
+        <th>content</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+    @foreach($diaries as $diary)
+      <tr>
+        <td class="align-middle">{{ $diary->writing_time }}</td>
+        <td class="align-middle">{{ $diary->title }}</td>
+        <td class="align-middle">{{ $diary->contents }}</td>
+        <td class="btn btn-primary"><a class="btn" href="diary/{{ $diary->id }}"><i class="fa fa-book"></i></a></td>
+      </tr>
+    @endforeach
+    </tbody>
+  </table>
+</div>
 
 @endsection
