871f51c0027d1989335206de13bffc59ab021df1
磯谷浩貴 <>
2019-10-07 18:31:00 +0900

    日記作成時にバリデーションによるエラーメッセージを表示できるように修正

--- resources/views/diary/create.blade.php
+++ resources/views/diary/create.blade.php
@@ -5,14 +5,17 @@
 <div class="container">
 {!! Form::open(['route' => 'diary.store']) !!}
   {!! Form::input('hidden', 'user_id', Auth::id(), ['class' => 'form-control']) !!}
-    <div class="form-group">
+    <div class="form-group @if(!empty($errors->first('writing_time'))) has-error @endif">
       {!! Form::input('date', 'writing_time', null, ['class' => 'form-control']) !!}
+      <span class="help-block">{{$errors->first('writing_time')}}</span>
     </div>
-    <div class="form-group">
-      {!! Form::input('text', 'title', null, ['class' => 'form-control', 'placeholder' => '日記内容']) !!}
+    <div class="form-group @if(!empty($errors->first('title'))) has-error @endif">
+      {!! Form::input('text', 'title', null, ['class' => 'form-control', 'placeholder' => 'title']) !!}
+      <span class="help-block">{{$errors->first('title')}}</span>
     </div>
-    <div class="">
+    <div class="form-group @if(!empty($errors->first('contents'))) has-error @endif">
           {!! Form::textarea('contents', null, ['class' => 'form-control', 'placeholder' => 'Content']) !!}
+          <span class="help-block">{{$errors->first('contents')}}</span>
     </div>
     <button type="submit" class="btn btn-success float-right">追加</button>
   {!! Form::close() !!}
