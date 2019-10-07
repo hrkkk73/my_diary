2c9aede09750036ac51a461fa0b621dce6c6c987
磯谷浩貴 <>
2019-10-07 18:18:25 +0900

    詳細ページの表示変更

--- resources/views/diary/show.blade.php
+++ resources/views/diary/show.blade.php
@@ -4,15 +4,14 @@
 <h1 class="page-header">日記詳細</h1>
 <div class="main-wrap">
   <div class="container">
-    {!! Form::input('hidden', 'user_id', Auth::id(), ['class' => 'form-control']) !!}
-      <div class="form-group">
-        {!! Form::input('text', 'writing_time', $diaries->writing_time->format('Y年m月d日'), ['class' => 'form-control']) !!}
+      <div class="form-group">Date
+        <p class="form-control">{{ $diaries->writing_time->format('Y年m月d日') }}</p>
       </div>
-      <div class="form-group">
-        {!! Form::input('text', 'title', $diaries->title, ['class' => 'form-control', 'placeholder' => '日記内容']) !!}
+      <div class="form-group">Title
+        <p class="form-control">{{ $diaries->title }}</p>
       </div>
-      <div class="form-group">
-        {!! Form::textarea('contents', $diaries->contents, ['class' => 'form-control', 'placeholder' => 'Content']) !!}
+      <div class="form-group">Content
+        <p class="form-control">{{ $diaries->contents }}</p>
       </div>
       <div class="form-inline float-right">
         <a class="mr-2 btn btn-success" href="{{ route('diary.edit', $diaries->id) }}">編集</a>
