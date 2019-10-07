09dbadad33a2d161e007af16d1a0674dbbdf5ad0
磯谷浩貴 <>
2019-10-07 14:59:05 +0900

    titleとcontentの未字数指定で折り返し設定、検索ボタン・詳細ボタン・日記を追加ボタンのmargin設定

--- resources/views/diary/index.blade.php
+++ resources/views/diary/index.blade.php
@@ -3,15 +3,15 @@
 
 <h1 class="page-header">日記一覧</h1>
 <div class="main-wrap diary">
-  <div class="btn-wrapper diary">
+  <div class="mr-3 form-inline float-right">
     {!! Form::open(['route' => 'diary.index', 'method' => 'GET']) !!}
       {!! Form::input('month', 'search-month', empty($inputs['search-month']) ? null : $inputs['search-month'], ['class' => 'form-control']) !!}
-      <button type="submit" class="btn"><i class="fa fa-search"></i></button>
+      <button type="submit" class="btn btn-success"><i class="fa fa-search"></i></button>
     {!! Form::close() !!}
-    <p class="text-right">
+  </div>
+    <p class="ml-3 text-left">
       <a class="btn btn-success" href="/diary/create">日記を追加</a>
     </p>
-  </div>
   <table class="table">
     <thead class="thead-light">
       <tr>
@@ -24,10 +24,10 @@
     <tbody>
     @foreach($diaries as $diary)
       <tr>
-        <td class="align-middle">{{ $diary->writing_time }}</td>
-        <td class="align-middle">{{ $diary->title }}</td>
-        <td class="align-middle">{{ $diary->contents }}</td>
-        <td class="btn btn-primary"><a class="btn" href="diary/{{ $diary->id }}"><i class="fa fa-book"></i></a></td>
+        <td class="align-middle">{{ $diary->writing_time->format('Y年m月d日') }}</td>
+        <td class="align-middle">{{ mb_strimwidth($diary->title, 0, 80, '...', 'UTF-8') }}</td>
+        <td class="align-middle">{{ mb_strimwidth($diary->contents, 0, 80, '...', 'UTF-8') }}</td>
+        <td class="btn btn-primary"><a class="btn" href="diary/{{ $diary->id }}">詳細</a></td>
       </tr>
     @endforeach
     </tbody>
