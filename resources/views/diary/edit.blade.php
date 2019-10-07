24d88e1cb20e83f2c9d93e47f55562329c535fa3
磯谷浩貴 <>
2019-10-07 10:27:04 +0900

    Formタグ内のルーティング設定 不要な一文削除

--- resources/views/diary/edit.blade.php
+++ resources/views/diary/edit.blade.php
@@ -4,14 +4,13 @@
 <h2 class="mb-3">日記編集</h2>
 <div class="main-wrap">
   <div class="container">
-    {!! Form::open() !!}
+    {!! Form::open(['route' => ['diary.update', $diary->id], 'method' => 'PUT']) !!}
       {!! Form::input('hidden', 'user_id', Auth::id(), ['class' => 'form-control']) !!}
       <div class="form-group">
         {!! Form::input('date', 'writing_time', $diary->writing_time, ['class' => 'form-control']) !!}
       </div>
       <div class="form-group">
         {!! Form::input('text', 'title', $diary->title, ['class' => 'form-control', 'placeholder' => '日記内容']) !!}
-        <!-- <input type="text" class="form-control" placeholder="日記内容"> -->
       </div>
       <div class="form-group">
         {!! Form::textarea('contents', $diary->contents, ['class' => 'form-control']) !!}
