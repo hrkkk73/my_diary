7cc4e0e529e832fbee76143a44cc064bf1bf6f51
磯谷浩貴 <>
2019-10-07 14:49:47 +0900

    詳細画面の表示変更、修正

--- resources/views/diary/show.blade.php
+++ resources/views/diary/show.blade.php
@@ -3,32 +3,25 @@
 
 <h1 class="page-header">日記詳細</h1>
 <div class="main-wrap">
-  <div class="panel panel-success">
-    <div class="panel-heading">
-      {{ $diaries->writing_time }}&nbsp;&nbsp;の日記
-    </div>
-    <div class="table-responsive">
-      <table class="table table-striped table-bordered">
-        <tbody>
-          <tr>
-            <th class="table-column">Title</th>
-            <td class="td-text">{{ $diaries->title }}</td>
-          </tr>
-          <tr>
-            <th class="table-column">Content</th>
-            <td class='td-text'>{!! nl2br(e($diaries->contents)) !!}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div>
-  <div class="btn-bottom-wrapper">
-    <a class="btn btn-edit" href="{{ route('diary.edit', $diaries->id) }}"><i class="fas fa-pencil-alt"></i></i></a>
-    <div class="btn-delete">
-      {!! Form::open(['route' => ['diary.user.delete', $diaries->id], 'method' => 'DELETE']) !!}
-        <button class="btn btn-danger" type="submit"><i class="fas fa-trash-alt"></i></i></button>
-      {!! Form::close() !!}
-    </div>
+  <div class="container">
+    {!! Form::input('hidden', 'user_id', Auth::id(), ['class' => 'form-control']) !!}
+      <div class="form-group">
+        {!! Form::input('text', 'writing_time', $diaries->writing_time->format('Y年m月d日'), ['class' => 'form-control']) !!}
+      </div>
+      <div class="form-group">
+        {!! Form::input('text', 'title', $diaries->title, ['class' => 'form-control', 'placeholder' => '日記内容']) !!}
+      </div>
+      <div class="form-group">
+        {!! Form::textarea('contents', $diaries->contents, ['class' => 'form-control', 'placeholder' => 'Content']) !!}
+      </div>
+      <div class="form-inline float-right">
+        <a class="mr-2 btn btn-success" href="{{ route('diary.edit', $diaries->id) }}">編集</a>
+        <div class="btn-delete">
+          {!! Form::open(['route' => ['diary.user.delete', $diaries->id], 'method' => 'DELETE']) !!}
+            <button class="btn btn-danger" type="submit">削除</button>
+          {!! Form::close() !!}
+        </div>
+      </div>
   </div>
 </div>
 
